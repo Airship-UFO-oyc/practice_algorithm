a90266eaad86e6155c633810c472861bb69e25a4
ouyangchun <ouyangchun_oyc@qq.com>
2018-05-02 08:37:25 +0800

    init

---
+++ src/main/java/com/airship/algorithm/lintcode/backpack/th000749_johns_backyard_garden/TH000749JohnsBackyardGarden.java
@@ -0,0 +1,34 @@
+package com.airship.algorithm.lintcode.backpack.th000749_johns_backyard_garden;
+
+/**
+ * 
+ * 749. John's backyard garden
+ * <p>
+ * https://www.lintcode.com/en/problem/johns-backyard-garden/
+ * <p>
+ * John wants to build a back garden on the empty space behind his home. There
+ * are two kinds of bricks now, one is 3 dm high and the other is 7 dm high.
+ * John wants to enclose a high x dm wall. If John can do this, output YES,
+ * otherwise NO.
+ * <p>
+ * Notice: X is an integer, and it's range is [3, 1000].
+ * 
+ * @author Airship 2018年5月2日
+ * @see [相关类/方法]
+ * @since 1.0
+ */
+public class TH000749JohnsBackyardGarden
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void action()
+    {
+
+    }
+
+}
